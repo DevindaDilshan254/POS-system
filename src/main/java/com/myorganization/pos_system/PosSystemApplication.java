cfae51f5cb4a0b9ffb5b95092677264320c90f70
DevindaDilshan254 <devindadilshan254@gmail.com>
2025-01-23 23:09:00 +0530

    Add swagger API testing tool from add third party dependencies that swagger2 and swagger ui ,then did the confuration part of it through the configuration class

--- src/main/java/com/myorganization/pos_system/PosSystemApplication.java
+++ src/main/java/com/myorganization/pos_system/PosSystemApplication.java
@@ -2,8 +2,10 @@ package com.myorganization.pos_system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2 //need the property spring.mvc.pathmatch.matching-strategy=ant_path_matcher
 public class PosSystemApplication {
 
 	public static void main(String[] args) {
